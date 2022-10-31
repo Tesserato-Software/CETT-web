9e88803d24f183d94bc56c783c0dcdbde1955645
Eduardo Henrique Lisboa Alves <henriqueeduardo63@gmail.com>
2022-10-31 19:56:59 -0300

    preto

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import { NavBar } from "../Components/Navbar/NavBar";
 import { Hierarchy } from "../Page/Hierarchy";
 import { HierarchyList } from "../Page/Hierarchy/List";
+import { Login } from './../Page/Login/index';
+
 
 export const MainRouts = () => {
     return (
@@ -21,11 +23,20 @@ export const MainRouts = () => {
                     }
                 />
                 <Route
+                    path="login"
+                    element={<Login/>}
+             
+                />
+
+
+
+                <Route
                     path="hierarchy"
                     element={<Hierarchy/>}
                 >
                     <Route path="list" element={<HierarchyList/>}/>
                 </Route>
+            
             </Routes>
         </>
     )
