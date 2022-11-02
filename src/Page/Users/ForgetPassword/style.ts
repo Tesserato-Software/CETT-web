461c6b477e62faa6636d5046302543ba80af1614
Guilherme Iuri <71740621+guiliuri13@users.noreply.github.com>
2022-11-02 15:59:27 -0300

    done forget password

--- src/Page/Users/ForgetPassword/style.ts
+++ src/Page/Users/ForgetPassword/style.ts
@@ -7,6 +7,34 @@ export const ForgetContainer = styled.div`
     height: calc(100vh - 90px);
     padding: 15vh 0;
     section{
-        
+        max-width: 40%;
+        h1{
+            font-size: 2.5rem;
+        }
+        p{
+            font-size: 1.5rem;
+        }
+        form{
+            margin-top: 4rem;
+            display: flex;
+            gap: 7rem;
+            input{
+                padding: .2rem .7rem;
+                border: none;
+                outline: none;
+                border-radius: 5px;
+                font-size: 1.6rem;
+            }
+            button{
+                border-radius: 8px;
+                padding: .2rem 2rem;
+                background-color: #E13763;
+                font-size: 1.6rem;
+                font-weight: 500;
+                color: #FFFF;    
+                outline: none;
+                border: none;            
+            }
+        }
     }
 `;
