dca01c1071c7e560cf3578aaf0ea4e3c1e55ef47
Guilherme Iuri <71740621+guiliuri13@users.noreply.github.com>
2022-11-13 20:23:45 -0300

    init login fix

--- src/Page/Login/style.ts
+++ src/Page/Login/style.ts
@@ -1,85 +1,71 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-
-
-export const LoginDiv = styled.div
-`
-.container{
-    display: flex;
-    flex-direction: column;
-    background-color: #fff;
-    width: 28rem;
-    height: 35rem;
-    margin-left: 3.5rem;
-    margin-top: 3.5rem;
-    background: radial-gradient(97.66% 97.66% at 50% 2.34%, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.2) 100%), radial-gradient(97.57% 210.75% at 0.9% 2.98%, rgba(255, 255, 255, 0.4) 0%, rgba(255, 255, 255, 0) 100%) ;
-    filter: drop-shadow(0px 11px 14px rgba(0, 0, 0, 0.25));
-    backdrop-filter: blur(8px);
-    border-radius: 20px;
-    margin-bottom: 3.5rem;
-}
-h1{
-    align-self: center;
-    margin-top: 2rem;
-    color: #fff;
-    font-size: 2rem;
-    font-weight: 500;
-    margin-bottom: 2rem;
-}
-
-form{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 2rem;
-}
-
-.form-group{
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 3.5rem;
-    color: #fff;   
+export const LoginDiv = styled.div`
+    width: 100%;
+    height: calc(100vh - 90px);
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    place-items: center;
+	.container {
+		display: flex;
+		flex-direction: column;
+		background-color: #fff;
+		width: 100%;
+		height: 100%;
+		background: radial-gradient(
+				97.66% 97.66% at 50% 2.34%,
+				rgba(255, 255, 255, 0) 0%,
+				rgba(255, 255, 255, 0.2) 100%
+			),
+			radial-gradient(
+				97.57% 210.75% at 0.9% 2.98%,
+				rgba(255, 255, 255, 0.4) 0%,
+				rgba(255, 255, 255, 0) 100%
+			);
+		filter: drop-shadow(0px 11px 14px rgba(0, 0, 0, 0.25));
+		backdrop-filter: blur(8px);
+		border-radius: 20px;
+		margin-bottom: 3.5rem;
+        h1 {
+            align-self: center;
+            margin-top: 2rem;
+            color: #fff;
+            font-size: 2rem;
+            font-weight: 500;
+            margin-bottom: 2rem;
+        }
     
-}
-.form-group input{
-    width: 20rem;
-    height: 2.5rem;
-    border-radius: 5px;
-    border: none;
-    padding-left: 1rem;
-    font-size: 1rem;
-}
-  
-.button-login{
-    margin-top: 4.3rem;
-    width: 10.5rem;
-    height: 2.6rem;
-    border-radius: 8px;
-    background-color: #E13763;
-    color: #fff;
-    font-bold: 500;
-    border: none;
-}
-
-
-
-
-
-
-
-
+        form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin-top: 2rem;
+        }
     
-
-   
-
-
-
-
-
-   
-
-
-
-
-
-`
+        .form-group {
+            display: flex;
+            flex-direction: column;
+            margin-bottom: 3.5rem;
+            color: #fff;
+        }
+        .form-group input {
+            width: 20rem;
+            height: 2.5rem;
+            border-radius: 5px;
+            border: none;
+            padding-left: 1rem;
+            font-size: 1rem;
+        }
+    
+        .button-login {
+            margin-top: 4.3rem;
+            width: 10.5rem;
+            height: 2.6rem;
+            border-radius: 8px;
+            background-color: #e13763;
+            color: #fff;
+            font-bold: 500;
+            border: none;
+        }
+	}
+`;
