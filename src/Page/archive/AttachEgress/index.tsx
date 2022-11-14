a465031b1bf2896d133096b0a6200df8a830e399
Guilherme Iuri <71740621+guiliuri13@users.noreply.github.com>
2022-11-14 19:20:22 -0300

    fix attach egress

--- src/Page/archive/AttachEgress/index.tsx
+++ src/Page/archive/AttachEgress/index.tsx
@@ -1,64 +1,71 @@
 import React from "react";
-import {useEffect,useState} from "react";
+import { useEffect, useState } from "react";
 import { EgressDiv } from "./styles";
 import { useParams } from "react-router-dom";
 import { api } from "./../../../services/api";
+import { toast } from "react-toastify";
 
-interface ArchivesId{
-	id:number;
+interface ArchivesId {
+	id: number;
 	onChangeArchive: any;
 }
 
 interface Archives {
 	id: number;
 }
-interface Egresses{
-	id: number
-	arq_id: number
-	name: string
-	archive_id: number
+interface Egresses {
+	id: number;
+	arq_id: number;
+	name: string;
+	archive_id: number;
 }
 export const AttachEgress = () => {
 	const { id } = useParams();
-	const [archives,setArchives] = useState<Archives>();
+	const [archives, setArchives] = useState<Archives>();
 	const [egresses, setEgresses] = useState<Egresses[]>([]);
 	const [egressId, setEgressId] = useState<Number[]>([]);
-	
+
 	useEffect(() => {
-		api.get(`/archive/dashboard-attach-egress/${id}`).then((response)=>{
-			setArchives(response.data.archives);
-			setEgresses(response.data.egresses);
+		api.get(`/archive/dashboard-attach-egress/${id}`)
+			.then((response) => {
+				setArchives(response.data.archives);
+				setEgresses(response.data.egresses);
+			})
+			.catch((err) => {
+				toast.error(
+					"Erro ao listar, verifique se este arquivo existe.",
+					{ theme: "colored" }
+				);
+				console.error(err);
+			});
+	}, []);
 
-		})
-		.catch((err) => console.log(err))
-	},[]);
+	useEffect(() => console.log("aaa ->", egressId), [egressId]);
 
-	
-	const onSubmit = ()=>{
-		if (egressId[0] === 0){
-			return alert("NÃO FOI POSSÍVEL ANEXAR, SELECIONE UM EGRESSO")
-		}
-		api.post(`/archive/attach-egress/${archives?.id}`,{
-			egress:egressId
-		})
-	}
-	console.log(archives)
+	const onSubmit = () => {
+		api.post(`/archive/attach-egress/${archives?.id}`, {
+			egress: egressId,
+		});
+	};
+
 	return (
 		<EgressDiv>
 			<div className="container">
-					<table className="table-archive">
-						<thead>
-							<tr>
-								<th>ID ARQUIVO</th>
-							</tr>
-						</thead>	
-						<tbody>
-							{archives?.id
-							&& <span>
-            	<strong>Id: </strong>
-							{archives?.id}</span>}							
-						</tbody>
-					</table>
+				<table className="table-archive">
+					<thead>
+						<tr>
+							<th>ID ARQUIVO</th>
+						</tr>
+					</thead>
+					<tbody>
+						{archives?.id && (
+							<span>
+								<strong>Id: </strong>
+								{archives?.id}
+							</span>
+						)}
+					</tbody>
+				</table>
 
 				<table>
 					<thead>
@@ -70,28 +77,32 @@ export const AttachEgress = () => {
 						</tr>
 					</thead>
 					<tbody>
-						{egresses.map((egresses) => {
-							return <tr className="tr-egress">
-								<td>{egresses.id}</td>
-								<td>{egresses.name}</td>
-								<td>{egresses.arq_id}</td>
-								<td><input type="checkbox"
-								onClick={()=> setEgressId(egressId.concat(egresses.id))}
-								value={egresses.id}
-								/></td>
-							</tr>
-							})
-
-						}
+						{egresses.map((egress) => {
+							return (
+								<tr className="tr-egress">
+									<td>{egress.id}</td>
+									<td>{egress.name}</td>
+									<td>{egress.arq_id}</td>
+									<td>
+										<input
+											type="checkbox"
+											onClick={() => {
+												if (egressId?.includes(egress.id)){
+													setEgressId(egressId?.filter((item) => item !== egress.id))
+												} else {
+													setEgressId(egressId?.concat(egress.id))
+												}
+											}}
+											defaultChecked={egressId?.includes(egress.id)}
+										/>
+									</td>
+								</tr>
+							);
+						})}
 					</tbody>
 				</table>
 				<button onClick={onSubmit}>Anexar</button>
-				
-		</div>
-			
-						
+			</div>
 		</EgressDiv>
-	)
-}	
-
-
+	);
+};
