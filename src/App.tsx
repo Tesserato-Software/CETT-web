50a235c1c7eab5ddf08ce5135313591afd22538b
Guilherme Iuri <71740621+guiliuri13@users.noreply.github.com>
2022-11-15 18:58:45 -0300

    add school list

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,21 @@ import { useNavigate } from 'react-router-dom'
 import GlobalTheme from './style'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import { api } from './services/api'
+
+export const userDataContext = createContext<{user: user | undefined}>({user: undefined})
 
 function App() {
     const [shouldResetPassword, setShouldResetPassword] = useState<boolean>(false)
     const [isDontLogged, setIsDontLogged] = useState<boolean>(false)
     const [isDisabled, setIsDisabled] = useState<boolean>(false)
-    const [user_id, setUser_id] = useState<number>(0)
+    const [userData, setUserData] = useState<user>()
     const navigate = useNavigate()
 
     useEffect(() => {
         api.get('/auth/get-user-data')
-        .then((response) => setUser_id(response.data.id))
+        .then((response) => setUserData(response.data))
         .catch((error) => {
             if(error.response?.data?.message === 'Unauthorized' && !["/login", "/"].includes(window.location.pathname)) {
                 setIsDontLogged(true)
@@ -28,20 +30,19 @@ function App() {
             }
 
             if (error.response?.data?.message === 'should_reset_password') {
-                const user_id = error.response.data.id !== undefined ? error.response.data.id : null 
                 setShouldResetPassword(true);
-                setUser_id(user_id);
+                setUserData(error.response.data)
                 navigate('/should-reset-password');
             }
         })
     }, [window.location.pathname])
 
     return (
-        <>
+        <userDataContext.Provider value={{user: userData}}>
             <ToastContainer />
             <GlobalTheme />        
-            <MainRouts shouldResetPassword={shouldResetPassword} user_id={user_id} isDisabled={isDisabled} isDontLogged={isDontLogged} />
-        </>
+            <MainRouts shouldResetPassword={shouldResetPassword} user_id={userData?.id} isDisabled={isDisabled} isDontLogged={isDontLogged} />
+        </userDataContext.Provider>
             
     )
 }
--- src/models/User.d.ts
+++ src/models/User.d.ts
@@ -1,40 +1,56 @@
-type hierarchy = {
-    id: number
-    school_id: number
-    name: string
-    can_update: boolean
-    can_delete: boolean
+declare interface Password {
+	id: number;
+	password: string;
+	user_id: number;
+	created_at: Date;
 }
 
-declare export type user = {
-    full_name: string
-    id: number
-    hierarchy: hierarchy[]
-    email: string
+declare interface Hirarchy {
+	id: number;
+	name: string;
+	can_delete: boolean;
+	school_id: number;
+	can_update: boolean;
+	can_enable_users: boolean;
 }
 
-declare export type formData = {
-    full_name: {
-        value: string
-        min: number
-        max: number
-    }
-    email: {
-        value: string
-        min: number
-        max: number
-    }
-    password: {
-        value: string
-        min: number
-        max: number
-    }
-    confirmPassword: {
-        value: string
-        min: number
-        max: number
-    }
-    hierarchy_id: {
-        value: number
-    }
+declare interface user {
+	id: number;
+	full_name: string;
+	email: string;
+	hierarchy_id: number;
+	school_id: number;
+	is_enabled: boolean;
+	password_id: number;
+	should_reset_password: boolean;
+	password: string;
+	is_super_user: boolean;
+	passwords: Password[];
+	hirarchy: Hirarchy;
 }
+
+declare type formData = {
+	full_name: {
+		value: string;
+		min: number;
+		max: number;
+	};
+	email: {
+		value: string;
+		min: number;
+		max: number;
+	};
+	password: {
+		value: string;
+		min: number;
+		max: number;
+	};
+	confirmPassword: {
+		value: string;
+		min: number;
+		max: number;
+	};
+	hierarchy_id: {
+		value: number;
+	};
+};
