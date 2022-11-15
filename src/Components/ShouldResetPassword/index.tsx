8d87290e3904e22694db41e9d9a2044e7624619e
zesanches <josemanoelmalassise@gmail.com>
2022-11-14 22:57:26 -0300

    add auth to endpoint

--- src/Components/ShouldResetPassword/index.tsx
+++ src/Components/ShouldResetPassword/index.tsx
@@ -14,7 +14,7 @@ export const ShouldResetPassword = ({ user_id }: any) => {
   
   const onSubmit = () => {
     setIsLoading(true)
-    api.post(`psw-fa/${user_id}`)
+    api.post(`/auth/psw-storage/${user_id}`)
     .then(() => {
       toast.success('Senha atualizada com sucesso!')
       navigate('/egress/list')
