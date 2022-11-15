6c9071c4faba3b6b6b13ea6441f0a07c4e6a3134
Eduardo Henrique Lisboa Alves <henriqueeduardo63@gmail.com>
2022-11-14 22:33:42 -0300

    dudu tradutor

--- src/Components/Navbar/titles.ts
+++ src/Components/Navbar/titles.ts
@@ -1,5 +1,45 @@
 export const titles: {
     [key: string]: string;
 } = {
-    "archive-create": "Criar arquivo"
+    "archive-create": "Criar arquivo",
+    "egress-list": "Lista de Egressos",
+    "egress-create": "Cadastrar Egresso",
+    "archive-list": "Lista de Arquivos",
+    "users-list": "Lista de Usuários",
+    "users-create": "Cadastrar Usuário",
+    "hierarchy-create": "Cadastrar Hierarquia",
+    "hierarchy-list": "Lista de Hierarquía",
+    "egress-dettach": "Desvincular Egresso",
+    "egress-attach": "Vincular Egresso",
+    "first-access": "Primeiro Acesso",
+    "forget-password": "Esqueci minha senha",
+    "egress-delete": "Deletar Egresso",
+    "hierarchy-update": "Atualizar Hierarquia",
+    "hierarchy-delete": "Deletar Hierarquia",
+    "hierarchy-add-user": "Adicionar Usuário",
+    "remove-user": "Remover Usuário",
+    "users-list-disabled": "Lista de Usuários Desativados",
+    "users-update": "Atualizar Usuário",
+    "hierachy-users-delete": "Remover Usuário",
+    "egress-edit": "Atualizar Egresso",
+    "archive-delete": "Deletar Arquivo",
+    "hierarchy-remove-user": "Remover Usuário",
+    "users-delete": "Deletar Usuário",
+    "school-list": "Lista de Escolas",
+    "school-create": "Cadastrar Escola",
+    "school-update": "Atualizar Escola",
+    "school-delete": "Deletar Escola",
+
+
+    
+
+    
+
+
+
+    
+
+    
+
+
 }
