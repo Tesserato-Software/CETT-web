2edbf38698ad4361a57c5de92b97b51d39746032
BigodeGui08 <113439170+BigodeGui08@users.noreply.github.com>
2022-11-14 22:14:50 -0300

    List

--- src/Page/archive/list/index.tsx
+++ src/Page/archive/list/index.tsx
@@ -1,34 +1,38 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 import { api } from '../../../services/api'
 import { ListArchiveContainer } from './style'
 
+interface ListArchive {  /* A gente ta tipando o que vai receber do back*/
+  id: number;
+}
+
 export const ListArchive = () => {
-  const [archiveData, setArchiveData] = useState()
+  const [archiveData, setArchiveData] = useState<ListArchive[]>([]) /* Aqui para */
   useEffect(() => {
-  api.get('archive/list')
-  .then((response)=> {
-    setArchiveData(response.data) 
-      console.log(response.data)
-  })
-  },[])
+    api.get('archive/list')
+      .then((response) => {
+        setArchiveData(response.data)
+        console.log(response.data)
+      })
+  }, [])
   return (
     <ListArchiveContainer>
-        <div className="conteudo">
-          <div>
-            <div>
-              <h2>Caixa [id]</h2>  <span>[id menor] - [id maior</span>
-            </div>
+      <div className="conteudo">
+        <div>
+          {archiveData.map((archive) => (
 
-            <div>
-              <h2>Caixa [id]</h2>  <span>[id menor] - [id maior</span>
-            </div>
+            <Link to={`/archive/delete/${archive.id}`} replace={true}>
+              <h2>Caixa {archive.id} </h2>
+            </Link>
 
-            <div>
-              <h2>Caixa [id]</h2>  <span>[id menor] - [id maior</span>
-            </div>
-           </div> 
+          ))}
+
+
+          { }
         </div>
-        
+      </div>
+
     </ListArchiveContainer>
   )
 }
