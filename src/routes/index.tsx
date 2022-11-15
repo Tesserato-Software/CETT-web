730c8e58e7c5b96c52f3aa41c4403a3a8821e446
zesanches <josemanoelmalassise@gmail.com>
2022-11-15 20:24:19 -0300

    add unauthorized component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -51,11 +51,11 @@ export const MainRouts = ({
 	isDisabled,
 	isDontLogged,
     user_hierarchy
-}: {
+}:{ 
     shouldResetPassword: boolean,
-	user_id: number,
-	isDisabled: boolean,
-	isDontLogged: boolean,
+    user_id: number,
+    isDisabled: boolean,
+    isDontLogged: boolean,
     user_hierarchy: hierarchy | undefined
 }) => {
 	const arr = window.location.pathname.split("/"),
@@ -66,7 +66,7 @@ export const MainRouts = ({
             return user_hierarchy[permission]
         }
         return false
-    } 
+    }
 
 	const ValidateUser = () => {
 		if (isDontLogged) {
@@ -123,14 +123,22 @@ export const MainRouts = ({
                         <Route path="egress" element={<Egress />}>
                             <Route path="list" element={<EgressList />} />
                             <Route path="create" element={<CreateEgress />} />
-                            <Route path="edit" element={<UpdateEgress />} 
+                            <Route path="edit" element={hasPermission("can_update") 
+                            ? <UpdateEgress /> 
+                            : <Unauthorized />} 
                             />
-                            <Route path="delete/:id" element={<DeleteEgress />} 
+                            <Route path="delete/:id" element={hasPermission("can_delete") 
+                            ? <DeleteEgress /> 
+                            : <Unauthorized />} 
                             />
 
                             {/* VINC BY ARCHIVE */}
-                            <Route path="dettach/:id" element={<DetachArchive />} />
-                            <Route path="attach/:id" element={<AttachArchive />} />
+                            <Route path="dettach/:id" element={hasPermission("can_update") 
+                            ? <DetachArchive /> 
+                            : <Unauthorized />} />
+                            <Route path="attach/:id" element={hasPermission("can_update") 
+                            ? <AttachArchive /> 
+                            : <Unauthorized />} />
                         </Route>
 
                         <Route path="excel" element={<Excel />}>
@@ -142,27 +150,41 @@ export const MainRouts = ({
                         <Route path="archive" element={<Archives />}>
                             <Route path="create" element={<CreateArchive />} />
                             <Route path="list" element={<ListArchive />} />
-                            <Route path="delete/:id" element={<DeleteArchive />} />
+                            <Route path="delete/:id" element={hasPermission("can_delete") 
+                            ? <DeleteArchive /> 
+                            : <Unauthorized />} />
 
                             {/* VINC BY EGRESS */}
                             <Route
                                 path="attach-egress/:id"
-                                element={<AttachEgress />}
+                                element={hasPermission("can_update")
+                                ? <AttachEgress /> 
+                                : <Unauthorized />}
                             />
-                            <Route path="detach-egress/:id" element={<DetachEgress />} 
+                            <Route path="detach-egress/:id" element={hasPermission("can_update")
+                            ? <DetachEgress /> 
+                            : <Unauthorized />} 
                             />
                         </Route>
 
                         <Route path="hierarchy" element={<Hierarchy />}>
                             <Route path="list" element={<HierarchyList />} />
                             <Route path="create" element={<CreateHierarchy />} />
-                            <Route path="update/:id" element={<UpdateHierarchy />} 
+                            <Route path="update/:id" element={hasPermission("can_update") 
+                            ? <UpdateHierarchy /> 
+                            : <Unauthorized />} 
                             />
-                            <Route path="delete/:id" element={<DeleteHierarchy />} 
+                            <Route path="delete/:id" element={hasPermission("can_delete") 
+                            ? <DeleteHierarchy /> 
+                            : <Unauthorized />} 
                             />
-                            <Route path="add-user" element={<AttachUser />} 
+                            <Route path="add-user" element={hasPermission("can_update") 
+                            ? <AttachUser /> 
+                            : <Unauthorized />} 
                             />
-                            <Route path="remove-user" element={<DettachUser />} 
+                            <Route path="remove-user" element={hasPermission("can_update") 
+                            ? <DettachUser /> 
+                            : <Unauthorized />} 
                             />
                         </Route>
 
@@ -173,8 +195,14 @@ export const MainRouts = ({
                                 path="list-disableds"
                                 element={<UsersListDisableds />}
                             />
-                            <Route path="update/:id" element={<UsersUpdate />} />
-                            <Route path="delete/:id" element={<UsersDelete />} />
+                            <Route path="update/:id" element={hasPermission("can_update") 
+                            ? <UsersUpdate /> 
+                            : <Unauthorized />} 
+                            />
+                            <Route path="delete/:id" element={hasPermission("can_delete") 
+                            ? <UsersDelete /> 
+                            : <Unauthorized />} 
+                            />
                         </Route>
                         <Route path="school" element={<School />}>
                             <Route path="list" element={<SchoolList />} />
