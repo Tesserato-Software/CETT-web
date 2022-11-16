627f3ed45e1b252d39fe3159acfb0956ae866d2e
Eduardo Henrique Lisboa Alves <henriqueeduardo63@gmail.com>
2022-11-16 14:42:42 -0300

    update edentação

--- src/pages/archive/list/index.tsx
+++ src/pages/archive/list/index.tsx
@@ -1,53 +1,62 @@
-import { ClassNames } from '@emotion/react';
-import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom';
-import { api } from '../../../services/api'
-import { ListArchiveContainer } from './style'
-import {useNavigate } from "react-router-dom";
+import { ClassNames } from "@emotion/react";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { api } from "../../../services/api";
+import { ListArchiveContainer } from "./style";
+import { useNavigate } from "react-router-dom";
 
-interface ListArchive {  /* A gente ta tipando o que vai receber do back*/
-  id: number;
+interface ListArchive {
+    /* A gente ta tipando o que vai receber do back*/ id: number;
 }
 
 export const ListArchive = () => {
-  const navigate = useNavigate();
-  const {id} = useParams();
-  const [archiveData, setArchiveData] = useState<ListArchive[]>([]) /* Aqui para */
-  useEffect(() => {
-    api.get('archive/list')
-      .then((response) => {
-        setArchiveData(response.data)
-        console.log(response.data)
-      })
-  }, [])
-  const onDelete = () =>{
-    navigate(`archive/delete/`)
-  }
-  const onAttach = () =>{
-    
-    navigate(`attach-egress/`)
-  }
-  return (
-    <ListArchiveContainer>
-      <div className="conteudo">
-        
-          {archiveData.map((archive) => (
-            
-            <div className='btn'><div></div><button><Link className='link-delete' to= {`/archive/delete/${archive.id}`}>DELETAR</Link></button>
-              <button><Link to= {`/archive/attach-egress/${archive.id}`}>ANEXAR</Link> </button>
-              <Link to={`/archive/detach-egress/${archive.id}`} replace={true}>
-                <h2>Caixa {archive.id} </h2>
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const [archiveData, setArchiveData] = useState<ListArchive[]>(
+        []
+    ); /* Aqui para */
+    useEffect(() => {
+        api.get("archive/list").then((response) => {
+            setArchiveData(response.data);
+            console.log(response.data);
+        });
+    }, []);
+    const onDelete = () => {
+        navigate(`archive/delete/`);
+    };
+    const onAttach = () => {
+        navigate(`attach-egress/`);
+    };
+    return (
+        <ListArchiveContainer>
+            <div className="conteudo">
+                {archiveData.map((archive) => (
+                    <div className="btn">
+                        <div></div>
+                        <button>
+                            <Link
+                                className="link-delete"
+                                to={`/archive/delete/${archive.id}`}
+                            >
+                                DELETAR
+                            </Link>
+                        </button>
+                        <button>
+                            <Link to={`/archive/attach-egress/${archive.id}`}>
+                                ANEXAR
+                            </Link>{" "}
+                        </button>
+                        <Link
+                            to={`/archive/detach-egress/${archive.id}`}
+                            replace={true}
+                        >
+                            <h2>Caixa {archive.id} </h2>
+                        </Link>
+                    </div>
+                ))}
 
-            </Link></div>
-            
-          ))}
-            
-
-          { }
-        </div>
-        
-      
-
-    </ListArchiveContainer>
-  )
-}
+                {}
+            </div>
+        </ListArchiveContainer>
+    );
+};
